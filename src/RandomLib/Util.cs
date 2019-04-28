bed1ca6194dc697c0f715a90f502ed09d7fe7ece
Chen Yu Pao <windperson@gmail.com>
2019-04-28 16:10:07 +0800

    Publish v1.0.2

--- src/RandomLib/Util.cs
+++ src/RandomLib/Util.cs
@@ -2,8 +2,16 @@
 
 namespace RandomLib
 {
+    /// <summary>
+    /// Utility class
+    /// </summary>
     public class Util
     {
+        /// <summary>
+        /// Create a default Math.NET Random seed source.
+        /// </summary>
+        /// <param name="threadSafe">set to true to prevent generate the same random value on multi-thread environment.</param>
+        /// <returns></returns>
         public static RandomSource DefaultRandomSource(bool threadSafe = false)
         {
             return new SystemRandomSource(RandomSeed.Guid(), threadSafe);
